cdaef1a034e45f4632a8e0b45b9425dff44d6ad6
RaveendraBhat <98217423+RaveendraBhat@users.noreply.github.com>
2022-11-21 00:08:22 +0530

    Add files via upload

---
+++ IMDb Web Scraping/scrapeIMDb.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests, openpyxl
+
+excel = openpyxl.Workbook()
+print(excel.sheetnames)
+sheet = excel.active
+sheet.title = 'Top 250 Movies'
+print(excel.sheetnames)
+sheet.append(['Movie Rank','Movie Name','Year of Release','IMDb Rating'])
+
+try:
+    source = requests.get('https://www.imdb.com/chart/top/?ref_=nv_mv_250')
+    source.raise_for_status()
+
+    soup = BeautifulSoup(source.text,'html.parser')
+    
+    movies = soup.find('tbody', class_="lister-list").find_all('tr')
+    
+    for movie in movies:
+        
+        rank = movie.find('td', class_="titleColumn").get_text(strip=True).split('.')[0]
+
+        name = movie.find('td', class_="titleColumn").a.text
+
+        year = movie.find('td', class_="titleColumn").span.text.strip('()')
+
+        rating = movie.find('td', class_="ratingColumn imdbRating").strong.text
+
+        print(rank, name,  year, rating)
+
+        sheet.append([rank, name,  year, rating])
+
+except Exception as e:
+    print(e)
+
+excel.save('IMDb Movie Ratings.xlsx')
